aaa5c205ff0204f5665158162d3c0f6b82bcb2e5
andela-nrotimi <rotimi.nelson@andela.com>
2017-03-21 14:38:13 +0100

    feature(webpack) integrate webpack with app

--- server/tests/api/roles.spec.js
+++ server/tests/api/roles.spec.js
@@ -1,18 +1,1 @@
-import chai from 'chai';
-import request from 'supertest';
-import db from '../../app/models';
-import app from '../../config/app';
 
-const superRequest = request.agent(app);
-const expect = chai.expect;
-describe('ROUTE GET /', () => {
-  it('should return a welcome message', (done) => {
-    superRequest.get('/')
-    .end((err, res) => {
-      expect(res.status).to.equal(200);
-      expect(res.body.message)
-      .to.equal();
-      done();
-    });
-  });
-});
